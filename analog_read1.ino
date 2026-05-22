b512d58bcc41be30504b6add657e231a23dd8769
Gayatri Shilpa <gayatrishilpaa@gmail.com>
2026-05-22 18:56:21 +0530

    Add files via upload

---
+++ analog_read1.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+int readPin=A3;
+int readVal;
+float v2 = 0.0;
+int delayTime=500;
+
+void setup()
+{
+  pinMode(readPin,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ readVal=analogRead(readPin);
+ v2=(5./1023.)*readVal; 
+ Serial.println(v2);
+ delay(delayTime); 
+}
